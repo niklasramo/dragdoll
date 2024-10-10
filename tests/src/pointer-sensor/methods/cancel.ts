a40ba6b711930b12fb743348eadadba1f383786c
Niklas Rämö <inramo@gmail.com>
2024-10-10 21:10:29 +0300

    Add test for pointer sensor's cancel method.

--- tests/src/pointer-sensor/methods/cancel.ts
+++ tests/src/pointer-sensor/methods/cancel.ts
@@ -1,5 +1,40 @@
+import { assert } from 'chai';
+import { createTestElement } from '../../utils/create-test-element.js';
+import { createFakeDrag } from '../../utils/create-fake-drag.js';
+import { PointerSensor } from '../../../../src/index.js';
+
 export function methodCancel() {
   describe('cancel', () => {
-    // TODO
+    it(`should cancel active drag forcefully`, () => {
+      const el = createTestElement();
+      const s = new PointerSensor(el, { sourceEvents: 'pointer' });
+      let cancelEventCount = 0;
+
+      s.on('cancel', () => {
+        ++cancelEventCount;
+      });
+
+      createFakeDrag(
+        [
+          { x: 1, y: 1 },
+          { x: 2, y: 2 },
+          { x: 2, y: 2 },
+        ],
+        {
+          eventType: 'pointer',
+          stepDuration: 0,
+          onAfterStep: () => {
+            assert.notEqual(s.drag, null);
+            s.cancel();
+          },
+        },
+      );
+
+      assert.equal(s.drag, null);
+      assert.equal(cancelEventCount, 1);
+
+      s.destroy();
+      el.remove();
+    });
   });
 }
