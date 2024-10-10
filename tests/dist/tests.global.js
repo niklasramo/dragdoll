ad0c56534bcfe4aedaaa22c524eb28569cfd17d7
Niklas Rämö <inramo@gmail.com>
2024-10-10 22:29:49 +0300

    Add test to pointer sensor drag property.

--- tests/dist/tests.global.js
+++ tests/dist/tests.global.js
@@ -8866,6 +8866,47 @@
         assert.equal(s.drag, null);
         s.destroy();
       });
+      it(`should contain drag data during drag`, function() {
+        const el = createTestElement();
+        const s = new PointerSensor(el, { sourceEvents: "pointer" });
+        let dragEventCount = 0;
+        createFakeDrag(
+          [
+            { x: 1, y: 1 },
+            { x: 2, y: 2 },
+            { x: 2, y: 2 }
+          ],
+          {
+            eventType: "pointer",
+            pointerId: 1,
+            pointerType: "touch",
+            stepDuration: 0,
+            onAfterStep: (e) => {
+              ++dragEventCount;
+              if (e.type === "start") {
+                assert.deepEqual(s.drag, {
+                  pointerId: 1,
+                  pointerType: "touch",
+                  x: 1,
+                  y: 1
+                });
+              } else if (e.type === "move") {
+                assert.deepEqual(s.drag, {
+                  pointerId: 1,
+                  pointerType: "touch",
+                  x: 2,
+                  y: 2
+                });
+              } else if (e.type === "end") {
+                assert.equal(s.drag, null);
+              }
+            }
+          }
+        );
+        assert.equal(dragEventCount, 3);
+        s.destroy();
+        el.remove();
+      });
     });
   }
 
